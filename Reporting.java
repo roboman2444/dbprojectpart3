d6ef1ed8c5b0cf7ec9a0b43f3015ee617a5a80a1
roboman2444 <roboman2444@gmail.com>
2017-02-22 23:34:36 -0500

    started queries

--- Reporting.java
+++ Reporting.java
@@ -1,10 +1,12 @@
 import java.sql.DriverManager;
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.ResultSet;
+
 import java.sql.SQLException;
 
 
 public class Reporting {
-
 
 	public static void Usage(){
 		System.out.printf(	"Usage: java Reporting <Username> <Password> <Mode>\n"+
@@ -13,7 +15,7 @@ public class Reporting {
 							"\t\t2- Report Doctors Basic Information\n"+
 							"\t\t3- Report Admissions Information\n"+
 							"\t\t4- Update Admissions Payment\n"+
-					"But before you run, you must set up various environment variables\n"+
+					"But before you compile or run, you must set up various environment variables\n"+
 						"\t> source /cs/bin/oracle-setup\n"+
 						"\t> export CLASSPATH=./:/usr/local/oracle11gr203/product/11.2.0/db_1/jdbc/lib/ojdbc6.jar\n"
 					);
@@ -21,8 +23,45 @@ public class Reporting {
 
 
 	public static void mode1(Connection connection){
+		System.out.printf("Enter Patient SSN:");
+		String ssn = System.console().readLine();
+		String q = "SELECT * FROM Patient WHERE SSN =" + ssn + "";
+		try {
+		Statement stmt = connection.createStatement();
+		ResultSet res = stmt.executeQuery(q);
+			if(res.next()){
+				System.out.printf(
+					"Patient SSN:%s\n" +
+					"Patient First Name:%s\n"+
+					"Patient Last Name:%s\n"+
+					"Patient Address:%s\n",
+					res.getString("SSN"),
+					res.getString("First_Name"),
+					res.getString("Last_Name"),
+					res.getString("Address"));
+			} else System.out.printf("No results\n");
+		} catch (SQLException e) { System.out.printf("error:%s\n", e.getMessage());}
 	}
 	public static void mode2(Connection connection){
+		System.out.printf("Enter Doctor ID:");
+		String id = System.console().readLine();
+		//TODOODODODODODO
+		String q = "SELECT * FROM Doctors WHERE SSN =" + id + "";
+		try {
+		Statement stmt = connection.createStatement();
+		ResultSet res = stmt.executeQuery(q);
+			if(res.next()){
+				System.out.printf(
+					"Doctor ID:%s\n" +
+					"Doctor First Name:%s\n"+
+					"Doctor Last Name:%s\n"+
+					"Doctor Gender:%s\n",
+					res.getString("ID"),
+					res.getString("First_Name"),
+					res.getString("Last_Name"),
+					res.getString("Gender"));
+			} else System.out.printf("No results\n");
+		} catch (SQLException e) { System.out.printf("error:%s\n", e.getMessage());}
 	}
 	public static void mode3(Connection connection){
 	}
